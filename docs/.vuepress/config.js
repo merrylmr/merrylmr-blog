c20495ec6bf1cfecfb22c0653e397e54fa8eb369
liaomeirong <merrylmr@qq.com>
2021-11-18 14:16:22 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: 'Merry 前端总结',
+    description: 'merrylmr 前端',
+    base: '/blog/',
+    configureWebpack: {}
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+'# Hello VuePress'
+
+::: tip
+这是一个提示
+:::
+
+[[toc]]
+
+
+
+
