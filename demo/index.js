948f942fdd81fa7b2451bb7f442a229094726d19
merry <merrylmr@qq.com>
2024-03-05 21:41:42 +0800

    防抖节流

--- demo/index.js
+++ demo/index.js
@@ -1,23 +1,27 @@
-export function throttle(fn, delay, options) {
-    let last = 0;
-    let timer = null;
-    return function (...args) {
-        const now = Date.now();
-        if (options && options.leading) {
-            if (now - last > delay) {
-                last = now;
-                fn.apply(this, args);
-            }
-        } else {
-            if (last && now < last + delay) {
-                clearTimeout(timer);
-                timer = setTimeout(() => {
-                    last = now;
-                    fn.apply(this, args);
-                }, delay);
-            }
-
-        }
-
+export function throttle(fn, delay) {
+    // 判断delay是否为数字
+    delay = typeof delay === 'number' ? delay : 0;
+    if (typeof fn !== 'function') {
+        throw new TypeError('Expected a function');
     }
+    let last = 0;
+
+    return function () {
+        const now = Date.now()
+        if (now - last > delay) {
+            last = now
+            fn.apply(this, arguments)
+        }
+    }
+}
+
+
+export function debounce(fn, delay) {
+    let timer = null;
+    return function () {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(this, arguments);
+        }, delay);
+    };
 }
